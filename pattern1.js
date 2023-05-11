a9e98961fe608e7dd633d3debf1ff4ecf358449b
sai-ramani <92101553+sai-ramani@users.noreply.github.com>
2023-05-11 11:22:50 +0530

    Create pattern1.js

---
+++ pattern1.js
@@ -0,0 +1,14 @@
+// write a program to print the specified pattern
+/*   *
+     **
+     ***
+     ****
+     *****
+*/
+for(var i=1;i<=5;i++){
+    s = '';
+    for(var j=1;j<=i;j++){
+        s += '*';
+    }
+    console.log(s);
+}
